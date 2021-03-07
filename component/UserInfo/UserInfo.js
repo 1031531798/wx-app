55e370a0512abcc6c6e8e9f12c739f7e3ca13466
1031531798 <1031531798@qq.com>
2021-03-08 00:05:39 +0800

    更新
    更新保存

---
+++ component/UserInfo/UserInfo.js
@@ -0,0 +1,23 @@
+// component/UserInfo/UserInfo.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    userInfo: {}
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
